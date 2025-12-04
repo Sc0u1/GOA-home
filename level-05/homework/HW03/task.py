ef0bf52d792ba2f35fadf2a89c44039ebf0d9130
Sc0u1 <KSHABURISHVILI@SEU.EDU.GE>
2025-12-04 21:12:37 +0400

    commit

--- level-05/homework/HW03/task.py
+++ level-05/homework/HW03/task.py
@@ -13,6 +13,21 @@ age: age
 birth: დღე/თვე/წელი
 ======================
 
-
-
 '''
+
+
+name = input("Enter your name: ")
+last_name = input("Enter your last name: ")
+birth_day = input('Enter your birth day as "DD": ')
+birth_month = input('Enter your birth month as "MM": ')
+birth_year = int(input('Enter your birth year as "YYYY": '))
+
+current_year = 2025
+age = current_year - birth_year
+
+print("======================")
+print("name: " + name)
+print("last name: " + last_name)
+print("age: " + age)
+print("birth: " + birth_day + "/" + birth_month + "/" + str(birth_year))
+print("======================")
